3a00199b7ef416318634c582072565d7cf1bb6dd
chethan0963 <chalamalachethansai963@gmail.com>
2025-09-10 10:29:49 +0530

    commiting if ATN.java

---
+++ ATN.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int number1 = 15;
+        int number2 = 25;
+        int sum = number1 + number2;
+        System.out.println("The first number is: " + number1);
+        System.out.println("The second number is: " + number2);
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+}
